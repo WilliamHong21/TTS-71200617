9385d6161e2a37f2820e4771891c97e0332d920c
WilliamHong21 <78341935+WilliamHong21@users.noreply.github.com>
2022-04-04 16:37:42 +0700

    Add files via upload

---
+++ Gamein/src/main/java/org/example/Actor.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Actor {
+    private float xpos;
+    private float ypos;
+
+    public void setXpos(int i) {
+    }
+
+    public void setYpos(int i) {
+    }
+
+}
